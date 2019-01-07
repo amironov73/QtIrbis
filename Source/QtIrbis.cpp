1f3ef2478e936c7d0143cc244268cd7b594a2a54
Alexey Mironov <amironov73@gmail.com>
2019-01-07 14:52:04 +0800

    First steps

--- Source/QtIrbis.cpp
+++ Source/QtIrbis.cpp
@@ -1,2 +1,195 @@
 #include "QtIrbis.h"
 
+//=========================================================
+
+SubField::SubField(char code)  : value() {
+    this->code = code;
+}
+
+SubField::SubField(char code, QString value) {
+    this->code = code;
+    this->value = value;
+}
+
+SubField* SubField::clone() const {
+    return new SubField(code, value);
+}
+
+QString SubField::toString() const {
+    return QString("^%d%s").arg(code).arg(value);
+}
+
+//=========================================================
+
+RecordField::RecordField(int tag) : value(), subfields() {
+    this->tag = tag;
+}
+
+RecordField::RecordField(int tag, QString value) : subfields() {
+    this->tag = tag;
+    this->value = value;
+}
+
+RecordField* RecordField::clone() const {
+    RecordField *result = new RecordField(tag, value);
+    foreach (const SubField *item, subfields) {
+        result->subfields.append(item->clone());
+    }
+
+    return result;
+}
+
+QString RecordField::toString() const {
+    QString result = QString("%d%s").arg(tag).arg(value);
+    foreach (const SubField *item, subfields) {
+        result.append(item->toString());
+    }
+
+    return result;
+}
+
+//=========================================================
+
+MarcRecord* MarcRecord::clone() const {
+    MarcRecord *result = new MarcRecord();
+    result->mfn = mfn;
+    foreach(const RecordField *item, fields) {
+        result->fields.append(item->clone());
+    }
+
+    return result;
+}
+
+QString MarcRecord::toString() const {
+    QString result;
+    bool first = true;
+    foreach (const RecordField *item, fields) {
+        if (first) {
+            result = item->toString();
+            first = false;
+        }
+        else {
+            result = result + "\n" + item->toString();
+        }
+    }
+
+    return result;
+}
+
+//=========================================================
+
+FileSpecification::FileSpecification(int path, QString database, QString filename) : content() {
+    this->path = path;
+    this->database = database;
+    this->filename = filename;
+}
+
+//=========================================================
+
+QString FileSpecification::toString() const {
+    QString result = filename;
+
+    switch (path) {
+    case 0:
+    case 1:
+        result = QString("%d..").arg(path) + result;
+        break;
+
+    default:
+        result = QString("%d.%s.").arg(path).arg(database) + result;
+        break;
+    }
+
+    return result;
+}
+
+//=========================================================
+
+IrbisConnection::IrbisConnection() :host("localhost"), port(6666),
+    username(), password(), database("IBIS"), workstation('C'),
+    clientId(0), queryId(0) {
+}
+
+void IrbisConnection::actualizeRecord(QString database, int mfn) {
+    // TODO implement
+}
+
+void IrbisConnection::connect() {
+    // TODO implement
+}
+
+void IrbisConnection::createDatabase(QString databaseName, QString description, bool readerAccess) {
+    // TODO implement
+}
+
+void IrbisConnection::createDictionary(QString databaseName) {
+    // TODO implement
+}
+
+void IrbisConnection::deleteDatabase(QString databaseName) {
+    // TODO implement
+}
+
+void IrbisConnection::deleteRecord(int mfn) {
+    // TODO implement
+}
+
+void IrbisConnection::disconnect() {
+    // TODO implement
+}
+
+//=========================================================
+
+ClientQuery::ClientQuery(IrbisConnection *connection, QString commandCode)
+    : buffer() {
+    this->connection = connection;
+    addAnsi(commandCode);
+    addAnsi(QString("%c").arg(connection->workstation));
+    addAnsi(commandCode);
+    add(connection->clientId);
+    connection->queryId++;
+    add(connection->queryId);
+    addAnsi(connection->password);
+    addAnsi(connection->username);
+    addLineFeed();
+    addLineFeed();
+    addLineFeed();
+}
+
+ClientQuery& ClientQuery::add(int value) {
+    return addAnsi(QString("%d").arg(value));
+}
+
+ClientQuery& ClientQuery::addAnsi(QString text) {
+    // TODO implement
+    return addLineFeed();
+}
+
+ClientQuery& ClientQuery::addLineFeed() {
+    char data[] = { '\x0A' };
+    buffer.write(data, 1);
+
+    return *this;
+}
+
+ClientQuery& ClientQuery::addUtf(QString text) {
+    // TODO implement
+    return addLineFeed();
+}
+
+char* ClientQuery::encode() {
+    // TODO implement
+    return NULL;
+}
+
+//=========================================================
+
+ServerResponse::ServerResponse() {
+    // TODO implement
+}
+
+void ServerResponse::close() {
+    // TODO implement
+}
+
+//=========================================================
--- Source/QtIrbis.h
+++ Source/QtIrbis.h
@@ -17,9 +17,13 @@ public:
     char code;
     QString value;
 
-    SubField() : code('\0'), value()
-    {
-    }
+    SubField() : code('\0'), value() {}
+    SubField(char code);
+    SubField(char code, QString value);
+
+    SubField* clone() const;
+
+    QString toString() const;
 };
 
 //=========================================================
@@ -29,10 +33,27 @@ class QTIRBIS_EXPORT RecordField
 public:
     int tag;
     QString value;
+    QVector<SubField*> subfields;
 
-    RecordField() : tag(0), value()
-    {
-    }
+    RecordField() : tag(0), value(), subfields() {}
+    RecordField(int tag);
+    RecordField(int tag, QString value);
+
+    RecordField* clone() const;
+
+    QString toString() const;
+};
+
+//=========================================================
+
+enum RecordStatus
+{
+    LogicallyDeleted = 1,
+    PhysicallyDeleted = 2,
+    Absent = 4,
+    NonActualized = 8,
+    Last = 32,
+    Locked = 64
 };
 
 //=========================================================
@@ -41,10 +62,155 @@ class QTIRBIS_EXPORT MarcRecord
 {
 public:
     int mfn;
+    QVector<RecordField*> fields;
 
-    MarcRecord() : mfn(0)
-    {
-    }
+    MarcRecord() : mfn(0), fields() {}
+
+    MarcRecord* clone() const;
+
+    QString toString() const;
 };
+
+//=========================================================
+
+enum IrbisPath
+{
+    System = 0,
+    Data = 1,
+    MasterFile = 2,
+    InvertedFile = 3,
+    ParameterFile = 10,
+    FullText = 11,
+    InternalResource = 12
+};
+
+//=========================================================
+
+class QTIRBIS_EXPORT FileSpecification
+{
+public:
+    int path;
+    QString database;
+    QString filename;
+    QString content;
+
+    FileSpecification(): path(), database(), filename(), content() {}
+    FileSpecification(int path, QString database, QString filename);
+
+    QString toString() const;
+};
+
+//=========================================================
+
+class QTIRBIS_EXPORT IrbisVersion
+{
+public:
+    QString organization;
+    QString version;
+    int maxClients;
+    int connectedClients;
+};
+
+//=========================================================
+
+class QTIRBIS_EXPORT IrbisProcessInfo
+{
+public:
+    QString number;
+    QString ipAddress;
+    QString name;
+    QString clientId;
+    QString workstation;
+    QString started;
+    QString lastCommand;
+    QString commandNumber;
+    QString processId;
+    QString state;
+};
+
+//=========================================================
+
+class QTIRBIS_EXPORT ClientInfo
+{
+public:
+    QString number;
+    QString ipAddress;
+    QString port;
+    QString name;
+    QString id;
+    QString workstation;
+    QString registered;
+    QString acknowledged;
+    QString lastCommand;
+    QString commandNumber;
+};
+
+//=========================================================
+
+class QTIRBIS_EXPORT DatabaseInfo
+{
+public:
+    QString name;
+    QString description;
+};
+
+//=========================================================
+
+class QTIRBIS_EXPORT IrbisConnection
+{
+public:
+    QString host;
+    int port;
+    QString username;
+    QString password;
+    QString database;
+    char workstation;
+    int clientId;
+    int queryId;
+
+    IrbisConnection();
+    void actualizeRecord(QString database, int mfn);
+    void connect();
+    void createDatabase(QString databaseName, QString description, bool readerAccess);
+    void createDictionary(QString databaseName);
+    void deleteDatabase(QString databaseName);
+    void deleteRecord(int mfn);
+    void disconnect();
+};
+
+//=========================================================
+
+class ClientQuery
+{
+private:
+    IrbisConnection *connection;
+    QBuffer buffer;
+
+public:
+    ClientQuery(IrbisConnection *connection, QString commandCode);
+
+    ClientQuery& add(int value);
+    ClientQuery& addAnsi(QString text);
+    ClientQuery& addLineFeed();
+    ClientQuery& addUtf(QString text);
+    char* encode();
+};
+
+//=========================================================
+
+class ServerResponse
+{
+public:
+    QString command;
+    int clientId;
+    int queryId;
+    int returnCode;
+
+    ServerResponse();
+
+    void close();
+};
+
+//=========================================================
 
 #endif // QTIRBIS_H
